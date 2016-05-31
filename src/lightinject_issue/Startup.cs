50733454e330a13bed1cb709b7f6bcc53b75f780
Bernhard Richter <bernhard.richter@gmail.com>
2016-05-31 19:53:16 +0200

    Using latest RC2 bits from NuGet

--- src/lightinject_issue/Startup.cs
+++ src/lightinject_issue/Startup.cs
@@ -12,8 +12,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
 
 namespace lightinject_issue
 {
