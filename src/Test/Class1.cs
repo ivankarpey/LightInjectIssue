0c20e039f423ffcfe795a48faad7a21f624e709f
Ivan Karpey <phoenix@open.by>
2016-07-08 20:38:26 +0300

    issue with context

---
+++ src/Test/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using lightinject_issue;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace Test
+{
+    public class Class1
+    {
+        private HttpClient client;
+
+        [Fact]
+        public async void Test1()
+        {
+            TestServer server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            client = server.CreateClient();
+
+            Task<HttpResponseMessage>[] tasks = new[]
+            {
+                client.GetAsync("/1/scope_error"),
+                client.GetAsync("/1/scope_error"),
+                client.GetAsync("/1/scope_error"),
+                client.GetAsync("/1/scope_error")
+            };
+
+            Task.WaitAll(tasks);
+            Assert.True(tasks[0].Result.IsSuccessStatusCode);
+            Assert.True(tasks[1].Result.IsSuccessStatusCode);
+            Assert.True(tasks[2].Result.IsSuccessStatusCode);
+            Assert.True(tasks[3].Result.IsSuccessStatusCode);
+        }
+
+        [Fact]
+        public async void Test2()
+        {
+            TestServer server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            client = server.CreateClient();
+
+            Task<HttpResponseMessage>[] tasks = new[]
+            {
+                client.GetAsync("/1/scope_error"),
+                client.GetAsync("/1/scope_error"),
+                client.GetAsync("/1/scope_error"),
+                client.GetAsync("/1/scope_error")
+            };
+
+            Task.WaitAll(tasks);
+            Assert.True(tasks[0].Result.IsSuccessStatusCode);
+            Assert.True(tasks[1].Result.IsSuccessStatusCode);
+            Assert.True(tasks[2].Result.IsSuccessStatusCode);
+            Assert.True(tasks[3].Result.IsSuccessStatusCode);
+        }
+    }
+}
--- src/lightinject_issue/Startup.cs
+++ src/lightinject_issue/Startup.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
-using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Threading.Tasks;
 using LightInject;
 using LightInject.Microsoft.DependencyInjection;
@@ -15,53 +15,102 @@ using Microsoft.Extensions.Logging;
 
 namespace lightinject_issue
 {
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public User()
+        {
+            Id = new Guid();
+        }
+    }
+
     public class MyContext : DbContext
     {
         public MyContext(DbContextOptions<MyContext> options)
             : base(options)
-        { }
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
     }
 
-    public interface IInt { };
-
-    public class IntImpl : IInt
+    public interface IIntService
     {
+        void Register(User u);
+    };
+
+    public class IntServiceImpl : IIntService
+    {
+        private MyContext _ctx;
+
+        public IntServiceImpl(MyContext ctx)
+        {
+            this._ctx = ctx;
+        }
+
+        public void Register(User u)
+        {
+            this._ctx.Users.Add(u);
+            this._ctx.SaveChangesAsync();
+        }
     }
 
     public class BaseController : Controller
     {
-        public IInt BaseImpl { get; set; }
+        public IIntService BaseImpl { get; set; }
     }
 
-    [Route("/1")]
+    [Route("1")]
     public class TestController : BaseController
     {
-        public IInt MyInt { get; set; }
-        protected IInt MyProtectedInt { get; set; }
-//        private MyContext _ctx;
-//
-//        public TestController(MyContext ctx)
-//        {
-//            _ctx = ctx;
-//        }
+        public IIntService MyIntService { get; set; }
+        protected IIntService MyProtectedIntService { get; set; }
 
         public async Task<JsonResult> Index()
         {
-            if (MyInt != null && MyProtectedInt != null && BaseImpl != null)
+            if (MyIntService != null && MyProtectedIntService != null && BaseImpl != null)
             {
                 return Json("success");
             }
             throw new NullReferenceException();
         }
+
+        [HttpGet("scope_error")]
+        public async Task<JsonResult> ScopeError()
+        {
+            List<Task> tasks = new List<Task>();
+            tasks.Add(Task.Run(() =>
+            {
+                ((dynamic)Startup.GetProvider().GetService(typeof(IIntService))).Register(new User());
+            }));
+            tasks.Add(Task.Run(() =>
+            {
+                ((dynamic)Startup.GetProvider().GetService(typeof(IIntService))).Register(new User());
+            }));
+            tasks.Add(Task.Run(() =>
+            {
+                ((dynamic)Startup.GetProvider().GetService(typeof(IIntService))).Register(new User());
+            }));
+            tasks.Add(Task.Run(() =>
+            {
+                ((dynamic)Startup.GetProvider().GetService(typeof(IIntService))).Register(new User());
+            }));
+            Task.WaitAll(tasks.ToArray());
+            return Json("success");
+        }
+
     }
 
     public class Startup
     {
+        private static IServiceProvider provider;
+        public static string TEST_ENV = "test";
         public IConfigurationRoot Configuration { get; }
 
         public Startup(IHostingEnvironment env)
         {
-
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath);
 
@@ -73,12 +122,25 @@ namespace lightinject_issue
             services.AddMvc();
             services.AddDbContext<MyContext>(options => options.UseInMemoryDatabase());
             ServiceContainer ctr = new ServiceContainer();
+            
+            //**********************************************************************************
+            ctr.SetDefaultLifetime<PerRequestLifeTime>(); //this line have no effect since this hard override in ctr.CreateServiceProvider
+            //**********************************************************************************
             ctr.RegisterAssembly(typeof(Startup).GetTypeInfo().Assembly);
-            //ctr.ScopeManagerProvider = new PerLogicalCallContextScopeManagerProvider();
-            //ctr.Register<MyContext>(new PerScopeLifetime());
             
-            
-            return ctr.CreateServiceProvider(services);
+            IServiceProvider provider = ctr.CreateServiceProvider(services);
+            SetProvider(provider);
+            return provider;
+        }
+
+        public static void SetProvider(IServiceProvider provider)
+        {
+            Startup.provider = provider;
+        }
+
+        public static IServiceProvider GetProvider()
+        {
+            return provider;
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -86,6 +148,15 @@ namespace lightinject_issue
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
             app.UseMvc();
+
+            //just to force ef infrastrcture configuration
+            var ctx = app.ApplicationServices.GetService<MyContext>();
+            using (ctx)
+            {
+                ctx.Users.Add(new User());
+                ctx.SaveChanges();
+            }
+
         }
     }
 }
